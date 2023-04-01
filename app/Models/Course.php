791053ebf89a470ec6924980dc788308c851fcf2
BagusFary <bagusfaryananta@gmail.com>
2023-04-01 11:36:05 +0700

    model update

--- app/Models/Course.php
+++ app/Models/Course.php
@@ -8,4 +8,19 @@ use Illuminate\Database\Eloquent\Model;
 class Course extends Model
 {
     use HasFactory;
+
+    public function orders()
+    {
+        return $this->hasMany(Order::class, 'course_id', 'id');
+    }
+
+    public function courseTag()
+    {
+        return $this->hasMany(CourseTag::class, 'course_id', 'id');
+    }
+
+    public function videos()
+    {
+        return $this->hasMany(Video::class, 'course_id', 'id');
+    }
 }
