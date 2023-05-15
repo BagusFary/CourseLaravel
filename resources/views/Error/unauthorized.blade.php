98f10b7941134a1a26921dfd35b5152eff0ee72a
BagusFary <bagusfaryananta@gmail.com>
2023-05-15 19:06:34 +0700

    update

--- resources/views/Error/unauthorized.blade.php
+++ resources/views/Error/unauthorized.blade.php
@@ -2,8 +2,13 @@
 
 @section('content')
     <div class="container">
-        <div class="d-flex justify-content-center mt-5">
+        {{-- <div class="d-flex justify-content-center mt-5">
             <h1>UNAUTHORIZED</h1>
-        </div>   
+        </div>    --}}
+                    <div class="text-center">
+                        <div class="error mx-auto" data-text="401">401</div>
+                        <p class="lead text-gray-800 mb-5">UNAUTHORIZED</p>
+                        <a href="/course" class="btn btn-outline-dark">Go Back</a>
+                    </div>
     </div>
 @endsection
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -13,6 +13,13 @@
     <link rel="dns-prefetch" href="//fonts.gstatic.com">
     <link href="https://fonts.bunny.net/css?family=Nunito" rel="stylesheet">
 
+      <!-- Custom fonts for this template-->
+      <link href="vendor/fontawesome-free/css/all.min.css" rel="stylesheet" type="text/css">
+      <link href="https://fonts.googleapis.com/css?family=Nunito:200,200i,300,300i,400,400i,600,600i,700,700i,800,800i,900,900i" rel="stylesheet">
+  
+      <!-- Custom styles for this template-->
+      <link href="css/sb-admin-2.min.css" rel="stylesheet">
+
     <!-- Scripts -->
     @vite(['resources/sass/app.scss', 'resources/js/app.js'])
 </head>
@@ -79,5 +86,15 @@
             @yield('content')
         </main>
     </div>
+
+        <!-- Bootstrap core JavaScript-->
+        <script src="vendor/jquery/jquery.min.js"></script>
+        <script src="vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
+    
+        <!-- Core plugin JavaScript-->
+        <script src="vendor/jquery-easing/jquery.easing.min.js"></script>
+    
+        <!-- Custom scripts for all pages-->
+        <script src="js/sb-admin-2.min.js"></script>
 </body>
 </html>
