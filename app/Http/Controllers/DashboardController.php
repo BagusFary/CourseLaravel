4a7f07b407602f5982ed5cff685a04ef39137a3f
BagusFary <bagusfaryananta@gmail.com>
2023-06-01 14:23:35 +0700

    update controller

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -43,7 +43,7 @@ class DashboardController extends Controller
     }
 
     public function showAllUserCourses(){
-        $dataCourse = 
-        return view('Dashboard.user.showallcourses', ['dataCourse' => $dataCourse]);
+        // $dataCourse = 
+        // return view('Dashboard.user.showallcourses', ['dataCourse' => $dataCourse]);
     }
 }
