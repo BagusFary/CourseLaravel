65d22d1773aefa830de1ffec471b3dc8fa0c5410
BagusFary <bagusfaryananta@gmail.com>
2023-06-01 21:57:15 +0700

    seeders update

---
+++ database/seeders/CourseSeeder.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+
+class CourseSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        // DB::table('courses')->insert([
+        //     "title" => "Product 1",
+        //     "description" => "Lorem ipsum dolor sit amet consectetur adipisicing elit. Rem molestiae, eligendi inventore alias quidem quisquam! Inventore rerum tempore rem nobis quia nemo omnis eos aliquid quos maiores, placeat enim doloribus eligendi temporibus repellat. Asperiores assumenda numquam molestias. Possimus excepturi minus iusto dolor saepe ex distinctio dolorem nam iure in recusandae eveniet autem eum sint unde voluptate laboriosam sapiente, quaerat obcaecati vitae nobis quibusdam? Quas necessitatibus magni unde veniam. Pariatur sit deleniti facilis quasi hic, magnam laborum eveniet excepturi, quaerat iusto exercitationem voluptatibus, amet harum doloremque minima. Saepe similique nulla exercitationem, dolorum magnam sed! Sed rem ipsa repellat unde dolor adipisci.",
+        //     "price" => 100000,
+        //     "thumbnail" => 
+        // ]);
+    }
+}
+
