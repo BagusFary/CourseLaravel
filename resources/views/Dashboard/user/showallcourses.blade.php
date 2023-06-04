5a74bfaf65f4846d9339e5164d068352ce03a27d
BagusFary <bagusfaryananta@gmail.com>
2023-06-04 14:19:56 +0700

    Views Update

--- resources/views/Dashboard/user/showallcourses.blade.php
+++ resources/views/Dashboard/user/showallcourses.blade.php
@@ -2,32 +2,30 @@
 
 @section('content')
 <div class="container">
-   <h2>Your Course:</h2>
-{{-- @foreach ($dataCourse as $item)
-    {{ $item }}
-@endforeach --}}
+ 
 
-@foreach ($dataCourse[0]->orders as $item)
-    {{ $item->course->title }}
-@endforeach
-   {{-- <div class="row">
-     @forelse ($dataCourse as $item)
+   <div class="row">
+     @forelse ($dataCourse->orders as $item)
+     <h2>Your Course:</h2>
         <div class="col-md-4 mb-5 mt-3 d-flex justify-content-center">
             <div class="card" style="width: 18rem;">
-                <img src="{{ asset('storage/thumbnail/'. $item->thumbnail) }}" class="card-img-top" alt="...">
+                <img src="{{ asset('storage/thumbnail/'. $item->course->thumbnail) }}" class="card-img-top" alt="...">
                 <div class="card-body">
-                  <h5 class="card-title">{{ $item->title }}</h5>
-                  <p class="card-text">{{ $item->description }}</p>
-                  <p>Rp.{{ number_format($item->price,2,',','.'); }}</p>
-                  <a href="#" class="btn btn-success">Open Course</a>
+                  <h5 class="card-title">{{ $item->course->title }}</h5>
+                  <p class="card-text">{{ $item->course->description }}</p>
+                  <a href="" class="btn btn-success">Open Course</a>
                 </div>
             </div>
         </div>
         @empty
-        <h2>No Course Found:(           
-            <a href="/course" class="btn btn-outline-dark">See more Course here</a>
-        </h2>
+        <div class="container d-flex justify-content-center mt-4">
+            <h2>No Course Found:(</h2>
+        </div>
+        <div class="container d-flex justify-content-center">
+            
+                <a href="/course" class="btn btn-outline-dark">See more Course here</a>
+        </div>
      @endforelse
-    </div> --}}
+    </div>
 </div>
 @endsection
