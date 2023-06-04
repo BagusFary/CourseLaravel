a5faa50d078df8d3372f4c5d8dd43dadef94c7f1
BagusFary <bagusfaryananta@gmail.com>
2023-06-04 14:21:51 +0700

    Routes Update

--- routes/web.php
+++ routes/web.php
@@ -4,6 +4,7 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\CourseController;
 use App\Http\Controllers\DashboardController;
 use App\Http\Controllers\TransactionController;
+use Illuminate\Support\Facades\Auth;
 
 /*
 |--------------------------------------------------------------------------
