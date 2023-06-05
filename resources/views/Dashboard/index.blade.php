3d0677ae10b78ea32150bad0c2faa51125d2b162
BagusFary <bagusfaryananta@gmail.com>
2023-06-05 21:12:46 +0700

    Views Update

--- resources/views/Dashboard/index.blade.php
+++ resources/views/Dashboard/index.blade.php
@@ -3,12 +3,15 @@
 @section('content')
 
     @can('admin')
-    <h1 class="h3 mb-4 text-gray-800">Welcome back , {{ Auth::user()->name }} </h1>
+    <h1 class="h3 text-gray-800">Welcome back , {{ Auth::user()->name }} </h1>
     @endcan
 
     @can('user')
-    <h1 class="h3 mb-4 text-gray-800">Welcome back , {{ Auth::user()->name }} </h1>
+    <h1 class="h3 text-gray-800">Welcome back , {{ Auth::user()->name }} </h1>
     @endcan
+
+    
+    
     
 
 @endsection
