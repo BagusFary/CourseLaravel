abaf9a54319e7dd441adfe8826b1dfcfabcbb77b
BagusFary <bagusfaryananta@gmail.com>
2023-06-12 01:02:34 +0700

    Controllers Update

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -79,6 +79,7 @@ class DashboardController extends Controller
         $invoiceData = Invoice::with(['orders:id,user_id,course_id,created_at','orders.user:id,name,email', 'orders.course:id,title,thumbnail,price'])
                             ->where('order_id', '=', $id)
                             ->get();
-        return view('Dashboard.user.invoicedetail', ['invoiceData' => $invoiceData]);
+
+            return view('Dashboard.user.invoicedetail', ['invoiceData' => $invoiceData]);   
     }
 }
