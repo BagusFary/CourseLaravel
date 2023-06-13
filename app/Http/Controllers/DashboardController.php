497f4ab2e0c85d377c3e3450fa9f4e6887c0bd95
Bagus Fary Ananta <51037655+BagusFary@users.noreply.github.com>
2023-06-13 12:16:12 +0700

    Update DashboardController.php

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -74,12 +74,15 @@ class DashboardController extends Controller
         }
     }
 
-    public function invoiceDetail($id){
+   public function invoiceDetail($id){
         
-        $invoiceData = Invoice::with(['orders:id,user_id,course_id,created_at','orders.user:id,name,email', 'orders.course:id,title,thumbnail,price'])
-                            ->where('order_id', '=', $id)
-                            ->get();
-
-            return view('Dashboard.user.invoicedetail', ['invoiceData' => $invoiceData]);   
+        if(Auth::user()->role == "user"){
+            $invoiceData = Invoice::with(['orders:id,user_id,course_id,created_at','orders.user:id,name,email', 'orders.course:id,title,thumbnail,price'])
+                                ->where('order_id', '=', $id)
+                                ->get();
+            return view('Dashboard.user.invoicedetail', ['invoiceData' => $invoiceData]);
+        } else {
+            return response()->view('Error.unauthorized');
+        }
     }
 }
