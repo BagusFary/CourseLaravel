ecfabe942c19af5b04ed01cb0f1bf88d2ffa7cdf
BagusFary <bagusfaryananta@gmail.com>
2023-06-17 15:52:15 +0700

    Models Update

--- app/Models/CourseTag.php
+++ app/Models/CourseTag.php
@@ -10,4 +10,12 @@ class CourseTag extends Model
     use HasFactory;
     protected $table = 'course_tag';
     protected $guarded = ['id'];
+
+
+    public function tags()
+    {
+        return $this->hasMany(Tags::class, 'tag_id', 'id');
+    }
 }
+
+
