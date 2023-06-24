635d71db177843607c5565106be399dab58326f2
BagusFary <bagusfaryananta@gmail.com>
2023-06-24 21:23:53 +0700

    Views Update

--- resources/views/Dashboard/user/invoicedetail.blade.php
+++ resources/views/Dashboard/user/invoicedetail.blade.php
@@ -2,14 +2,13 @@
 
 @section('content')
 <div class="container">
-    @foreach ($invoiceData as $item)
     
     <div class="card">
         <div class="card-body">
           <div class="container mb-5 mt-3">
             <div class="row d-flex align-items-baseline">
               <div class="col-xl-9">
-                <p style="color: #7e8d9f;font-size: 20px;">Invoice &gt;&gt; <strong>ID:{{ $item->id }} #</strong></p>
+                <p style="color: #7e8d9f;font-size: 20px;">Invoice &gt;&gt; <strong>ID:{{ $invoiceData->id }} #</strong></p>
               </div>
             </div>
             <div class="container">
@@ -22,26 +21,26 @@
               <div class="row">
                 <div class="col-xl-8">
                   <ul class="list-unstyled">
-                    <li class="text-muted">To: <span style="color:#8f8061 ;">{{ $item->orders->user->name }}</span></li>
+                    <li class="text-muted">To: <span style="color:#8f8061 ;">{{ $invoiceData->orders->user->name }}</span></li>
                     <li class="text-muted">Street, City</li>
                     <li class="text-muted">State, Country</li>
-                    <li class="text-muted"><i class="fa-regular fa-envelope"></i> {{ $item->orders->user->email }}</li>
+                    <li class="text-muted"><i class="fa-regular fa-envelope"></i> {{ $invoiceData->orders->user->email }}</li>
                   </ul>
                 </div>
                 <div class="col-xl-4">
                   <p class="text-muted">Invoice</p>
                   <ul class="list-unstyled">
                     <li class="text-muted"><i class="fas fa-circle" style="color:#8f8061 ;"></i> <span
-                        class="fw-bold">ID:</span>#{{ $item->id }}</li>
+                        class="fw-bold">ID:</span>#{{ $invoiceData->id }}</li>
                     <li class="text-muted"><i class="fas fa-circle" style="color:#8f8061 ;"></i> <span
-                        class="fw-bold">Creation Date: </span>{{ date("Y-m-d", strtotime($item->created_at))}}</li>
+                        class="fw-bold">Creation Date: </span>{{ date("Y-m-d", strtotime($invoiceData->created_at))}}</li>
                     <li class="text-muted"><i class="fas fa-circle" style="color:#8f8061;"></i> <span
                         class="me-1 fw-bold">Status:</span>
-                        @if ($item->status == "paid")
+                        @if ($invoiceData->status == "paid")
                             <span class="badge text-bg-success">Paid</span>  
-                        @elseif($item->status == "unpaid")
+                        @elseif($invoiceData->status == "unpaid")
                             <span class="badge text-bg-warning">Unpaid</span>
-                            @elseif($item->status == "cancel")
+                            @elseif($invoiceData->status == "cancel")
                             <span class="badge text-bg-danger">Canceled</span>
                         @endif
                     </li>
@@ -57,7 +56,7 @@
                               overflow-hidden
                               d-block
                               " data-ripple-color="light">
-                    <img src="{{ asset('/storage/thumbnail/'. $item->orders->course->thumbnail) }}"
+                    <img src="{{ asset('/storage/thumbnail/'. $invoiceData->orders->course->thumbnail) }}"
                       class="w-100" height="90px"  />
                     <a href="#!">
                       <div class="hover-overlay">
@@ -67,20 +66,20 @@
                   </div>
                 </div>
                 <div class="col-md-7 mb-4 mb-md-0">
-                  <p class="fw-bold">{{ $item->orders->course->title }}</p>
+                  <p class="fw-bold">{{ $invoiceData->orders->course->title }}</p>
                   <p class="mb-1">
-                    <span class="text-muted me-2">Order ID:</span><span>{{ $item->orders->id }}</span>
+                    <span class="text-muted me-2">Order ID:</span><span>{{ $invoiceData->orders->id }}</span>
                   </p>
                   <p>
-                    <span class="text-muted me-2">Order Date:</span><span>{{ date("Y-m-d", strtotime($item->orders->created_at)) }}</span>
+                    <span class="text-muted me-2">Order Date:</span><span>{{ date("Y-m-d", strtotime($invoiceData->orders->created_at)) }}</span>
                   </p>
                 </div>
                 <div class="col-md-3 mb-4 mb-md-0">
                   <h5 class="mb-2">
-                    @if ($item->status == "cancel")
-                    <s class="align-middle">Rp.{{ number_format($item->orders->course->price,2,',','.'); }}</s>
-                    @elseif($item->status != "cancel")
-                    <span class="align-middle">Rp.{{ number_format($item->orders->course->price,2,',','.'); }}</span>
+                    @if ($invoiceData->status == "cancel")
+                    <s class="align-middle">Rp.{{ number_format($invoiceData->orders->course->price,2,',','.'); }}</s>
+                    @elseif($invoiceData->status != "cancel")
+                    <span class="align-middle">Rp.{{ number_format($invoiceData->orders->course->price,2,',','.'); }}</span>
                     @endif
                   </h5>
                 </div>
@@ -91,12 +90,12 @@
                   <p class="ms-3">Add additional notes and payment information</p>
                 </div>
                 <div class="col-xl-3">
-                    @if ($item->status == "cancel")
+                    @if ($invoiceData->status == "cancel")
                     <s class="text-black float-start"><span class="text-black me-3"> Total Amount</span><span
-                        style="font-size: 17px;">Rp.{{ number_format($item->orders->course->price,2,',','.'); }}</span></s>
-                    @elseif($item->status != "cancel")
+                        style="font-size: 17px;">Rp.{{ number_format($invoiceData->orders->course->price,2,',','.'); }}</span></s>
+                    @elseif($invoiceData->status != "cancel")
                     <p class="text-black float-start"><span class="text-black me-3"> Total Amount</span><span
-                        style="font-size: 17px;">Rp.{{ number_format($item->orders->course->price,2,',','.'); }}</span></p>
+                        style="font-size: 17px;">Rp.{{ number_format($invoiceData->orders->course->price,2,',','.'); }}</span></p>
                     @endif
                 </div>
               </div>
@@ -104,6 +103,6 @@
           </div>
         </div>
       </div>
-      @endforeach
+      
 </div>
 @endsection
