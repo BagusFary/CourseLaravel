68c958d6e4d212fe5be6221424be2798a6ca7005
BagusFary <bagusfaryananta@gmail.com>
2023-06-25 10:53:13 +0700

    Views Update

--- resources/views/Error/coursenotfound.blade.php
+++ resources/views/Error/coursenotfound.blade.php
@@ -5,7 +5,7 @@
         <div class="text-center">
             <div class="error mx-auto" data-text="404">404</div>
             <p class="lead text-gray-800 mb-5">Course Not Found.</p>
-            <a href="{{ url()->previous() }}" class="btn btn-outline-dark">Go Back</a>
+            <a href="/course" class="btn btn-outline-dark">Go Back to Homepage</a>
         </div>
     </div>
 @endsection
--- resources/views/Error/notfound.blade.php
+++ resources/views/Error/notfound.blade.php
@@ -5,7 +5,7 @@
         <div class="text-center">
             <div class="error mx-auto" data-text="404">404</div>
             <p class="lead text-gray-800 mb-5">Not Found.</p>
-            <a href="{{ url()->previous() }}" class="btn btn-outline-dark">Go Back</a>
+            <a href="/dashboard" class="btn btn-outline-dark">Go Back</a>
         </div>
     </div>
 @endsection
