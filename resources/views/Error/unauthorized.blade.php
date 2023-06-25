633e5436963b1f374890f70e09d554ef3d4aa3e9
BagusFary <bagusfaryananta@gmail.com>
2023-06-25 10:58:20 +0700

    Views Update

--- resources/views/Error/unauthorized.blade.php
+++ resources/views/Error/unauthorized.blade.php
@@ -5,7 +5,7 @@
         <div class="text-center">
             <div class="error mx-auto" data-text="401">401</div>
             <p class="lead text-gray-800 mb-5">UNAUTHORIZED</p>
-            <a href="/dashboard" class="btn btn-outline-dark">Go Back</a>
+            <a href="/dashboard" class="btn btn-outline-dark">Go Back to Dashboard</a>
         </div>
     </div>
 @endsection
