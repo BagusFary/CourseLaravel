554d8a11d1a99b4000b2ba517a6ab48fca4cc0bb
BagusFary <bagusfaryananta@gmail.com>
2023-08-03 21:05:49 +0700

    Routes Update

--- routes/web.php
+++ routes/web.php
@@ -4,6 +4,7 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\CourseController;
 use App\Http\Controllers\DashboardController;
 use App\Http\Controllers\TransactionController;
+use App\Http\Livewire\EditCourse;
 use Illuminate\Support\Facades\Auth;
 
 /*
